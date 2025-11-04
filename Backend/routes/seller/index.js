0e3fdbf62f8f756ebb6487b40b43b2adbf1af0bd
kamleshkumwt12th@gmail.com <kamleshkumwt12th@gmail.com>
2025-11-04 20:11:13 +0530

    update  routes

--- Backend/routes/seller/index.js
+++ Backend/routes/seller/index.js
@@ -1,9 +1,11 @@
 import {Router} from 'express'
 import  authRoutes  from './auth.routes.js'
+import  sellerRoutes  from './seller.routes.js'
 
 const router = Router();
 
 router.use('/auth', authRoutes);
+router.use('/', sellerRoutes);
 
 
 export default router;
--- Backend/routes/user/index.js
+++ Backend/routes/user/index.js
@@ -1,9 +1,11 @@
 import {Router} from 'express'
 import  authRoutes  from './auth.routes.js'
+import  userRoutes  from './user.routes.js'
 
 const router = Router();
 
 router.use('/auth', authRoutes);
+router.use('/', userRoutes);
 
 
 export default router;
